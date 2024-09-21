97f49d3494ebac2acfb5836712fbf806080f5212
Yanlin Liu <yanlin.liuuu@outlook.com>
2024-09-21 20:53:57 +0200

    timer changes

--- Assets/script/UI/Timer.cs
+++ Assets/script/UI/Timer.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -5,6 +6,8 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Slider slider;
+    [SerializeField] Gradient gradient;
+    [SerializeField] Image fill;
 
     public float heatingTime;
     public bool countUp;
@@ -13,11 +16,28 @@ public class Timer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        slider.minValue = 0;
+        slider.maxValue = 20;
+
+        fill.color = gradient.Evaluate(0); //set the color of beginning.
     }
+
+
 
     // Update is called once per frame
     void Update()
+    {
+        HeatingUp();
+
+        // Different color of status
+        fill.color = gradient.Evaluate(slider.normalizedValue); // make the value 0-1.
+
+        //the pan is not heat enough
+
+
+    }
+
+    public void HeatingUp()
     {
         heatingTime = countUp ? heatingTime += Time.deltaTime : heatingTime;
         slider.value = heatingTime;
